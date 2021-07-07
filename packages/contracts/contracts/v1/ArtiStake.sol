f7edc9a1454add98d54e2cd49a7308c647ef145b
kenta suhara <ponta.blockchain@gmail.com>
2021-07-07 23:03:26 +0900

    feat: deposit and withdraw

--- packages/contracts/contracts/v1/ArtiStake.sol
+++ packages/contracts/contracts/v1/ArtiStake.sol
@@ -3,6 +3,9 @@
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
+import "hardhat/console.sol";
+import {WadRayMath} from "./libraries/math/WadRayMath.sol";
+
 interface IWETHGateway {
     function depositETH(
         address lendingPool,
@@ -19,6 +22,10 @@ interface IWETHGateway {
 
 interface IERC20 {
     function approve(address spender, uint256 amount) external returns (bool);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function scaledBalanceOf(address user) external view returns (uint256);
 }
 
 library DataTypes {
@@ -44,6 +51,8 @@ library DataTypes {
 
 interface ILendingPool {
     function getReserveData(address asset) external view returns (DataTypes.ReserveData memory);
+
+    function getReserveNormalizedIncome(address asset) external view returns (uint256);
 }
 
 /**
@@ -51,13 +60,18 @@ interface ILendingPool {
  * @dev Store & retrieve value in a variable
  */
 contract ArtiStake {
+    using WadRayMath for uint256;
+
     address payable public aaveLendingPool;
     address payable public aaveWETHGateway;
+    address public aTokenAddress;
+    address public underlyingAsset;
 
     uint256 constant MAX_UINT = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
 
     mapping(address => bool) public artistAddresses;
     mapping(address => mapping(address => uint256)) public depositedAmounts;
+    mapping(address => mapping(address => uint256)) public atokenAmounts;
 
     event Deposited(address indexed from, address indexed artistAddress, uint256 amount);
     event Withdrew(address indexed withdrawer, address indexed artistAddress, uint256 amount);
@@ -69,31 +83,57 @@ contract ArtiStake {
     ) public {
         aaveLendingPool = _aaveLendingPool;
         aaveWETHGateway = _aaveWETHGateway;
-        address aTokenAddress = ILendingPool(_aaveLendingPool).getReserveData(_currency).aTokenAddress;
+        underlyingAsset = _currency;
+        aTokenAddress = ILendingPool(_aaveLendingPool).getReserveData(_currency).aTokenAddress;
         IERC20(aTokenAddress).approve(aaveWETHGateway, MAX_UINT);
     }
 
     function deposit(
-        // address artistAddress,
+        address artistAddress,
         // uint256 amount,
         uint16 _referralCode
     ) public payable {
         // require(artistAddresses[artistAddress], "Artist not Registered");
         // Aaveにdeposit
+        uint256 contractBalanceBefore = IERC20(aTokenAddress).scaledBalanceOf(address(this));
         IWETHGateway(aaveWETHGateway).depositETH{value: msg.value}(aaveLendingPool, address(this), _referralCode);
-        // depositedAmounts[artistAddress][msg.sender] += amount;
+        uint256 contractBalanceAfter = IERC20(aTokenAddress).scaledBalanceOf(address(this));
+        atokenAmounts[artistAddress][msg.sender] += (contractBalanceAfter - contractBalanceBefore);
+        depositedAmounts[artistAddress][msg.sender] += msg.value;
+        console.log(atokenAmounts[artistAddress][msg.sender], "atokenAmounts[artistAddress][msg.sender]");
+        console.log(depositedAmounts[artistAddress][msg.sender], "depositedAmounts[artistAddress][msg.sender]");
         // emit Deposited(msg.sender, artistAddress, amount);
     }
 
-    function withdraw(uint256 amount) public {
+    function withdraw(address payable artistAddress, uint256 amount) public {
         // require(artistAddresses[artistAddress], "Artist not Registered");
         // require(depositedAmounts[artistAddress][msg.sender] > amount, "not enough deposited balance");
-        // Aaveからwithdraw
+        uint256 atokenAmount = atokenAmounts[artistAddress][msg.sender];
+        uint256 depositedAmount = depositedAmounts[artistAddress][msg.sender];
+        uint256 userBalanceWithInterest = atokenAmount.rayMul(
+            ILendingPool(aaveLendingPool).getReserveNormalizedIncome(underlyingAsset)
+        );
+        uint256 totalInterest = userBalanceWithInterest - depositedAmount;
+
+        console.log(atokenAmount, "atokenAmount");
+        console.log(depositedAmount, "depositedAmount");
+        console.log(userBalanceWithInterest, "userBalanceWithInterest");
+        console.log(totalInterest, "totalInterest");
+
         IWETHGateway(aaveWETHGateway).withdrawETH(aaveLendingPool, amount, address(this));
-        // address payable sender = payable(msg.sender);
-        // sender.transfer(amount);
-        // transfer
-        // depositedAmounts[artistAddress][msg.sender] -= amount;
+
+        uint256 artistInterest = (totalInterest * amount) / depositedAmount / 2;
+        uint256 stakerReward = amount - artistInterest;
+        console.log(artistInterest, "artistInterest");
+        console.log(stakerReward, "stakerReward");
+
+        artistAddress.transfer(artistInterest);
+        payable(msg.sender).transfer(stakerReward);
+
+        atokenAmounts[artistAddress][msg.sender] -= amount.rayDiv(
+            ILendingPool(aaveLendingPool).getReserveNormalizedIncome(underlyingAsset)
+        );
+        depositedAmounts[artistAddress][msg.sender] -= amount;
         // emit Withdrew(msg.sender, artistAddress, amount);
     }
 
--- packages/contracts/test/ArtiStake.spec.ts
+++ packages/contracts/test/ArtiStake.spec.ts
@@ -6,25 +6,32 @@ import {
   POLYGON_AAVE_LENDING_POOL_ADDRESS,
   POLYGON_AAVE_WETH_GATEWAY_ADDRESS,
 } from "../lib/constants";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 chai.use(solidity);
 const { expect } = chai;
 
 describe("ArtiStake", function () {
   let artiStakeContract: any
-  let signer;
+  let signer: SignerWithAddress;
+  let artist: SignerWithAddress
   this.beforeEach(async function () {
-    [signer] = await ethers.getSigners();
+    [signer, artist] = await ethers.getSigners();
     const ArtiStake = await ethers.getContractFactory("ArtiStake");
     artiStakeContract = await ArtiStake.deploy(POLYGON_AAVE_LENDING_POOL_ADDRESS, POLYGON_AAVE_WETH_GATEWAY_ADDRESS, POLYGON_WETH_ADDRESS);
   });
 
-  it("User can stake to Artist", async function () {  
+  it.only("User can stake to Artist", async function () {  
     // artiStakeContract.deposit()をする
+    const balanceBefore = await ethers.provider.getBalance(artiStakeContract.address);
+    expect(balanceBefore).to.equal(0)
     
-    await artiStakeContract.deposit(0, {value: 10000})
-
-    await artiStakeContract.withdraw(1000)
+    await artiStakeContract.deposit(artist.address, 0, {value: 100000000000000})
+    await ethers.provider.send("evm_increaseTime", [31536000])
+    await artiStakeContract.withdraw(artist.address, 100500000000)
+    const artistBalance = await ethers.provider.getBalance(artist.address);
+    console.log(artistBalance.toString( ), "aa")
+    // expect(balanceAfter > balanceBefore).to.be.true
     // artiStakeCOntractのdepositedAmountの変化をみる
   });
 
@@ -33,6 +40,15 @@ describe("ArtiStake", function () {
     // timestamp進める
     // witdrawする
     // matic増えてる
+  });
+
+  it("Deposit twice", async function () {      
+    await artiStakeContract.deposit(artist.address, 0, {value: 100000000000000})
+    console.log("-----------------")
+    await ethers.provider.send("evm_increaseTime", [3600000])
+    await artiStakeContract.deposit(artist.address, 0, {value: 100000000000000})
+    // expect(balanceAfter > balanceBefore).to.be.true
+    // artiStakeCOntractのdepositedAmountの変化をみる
   });
 
 
