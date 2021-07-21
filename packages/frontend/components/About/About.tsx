239647efecff9b7e3b3755cdbbf59ca4a145b566
kenta suhara <ponta.blockchain@gmail.com>
2021-07-21 15:35:22 +0900

    fix: update about design

--- packages/frontend/components/About/About.tsx
+++ packages/frontend/components/About/About.tsx
@@ -1,11 +1,10 @@
 const About = () => {
   return (
-    <div className="w-full bg-marimo-2 p-8">
-      <h2 className="text-center text-white mb-4">-EN-</h2>
-      <p className="text-white text-sm mb-4">
-        "ArtiStake" is a platform for supporting artists and an artist-based community.
-      </p>
-      <p className="text-white text-sm mb-4">
+    <div className="w-full bg-marimo-1 py-8 px-40">
+      <h2 className="text-center text-2xl text-white my-8">About ArtiStake</h2>
+      <h3 className="text-white text-xl mb-4">-EN-</h3>
+      <p className="text-white mb-4">"ArtiStake" is a platform for supporting artists and an artist-based community.</p>
+      <p className="text-white mb-4">
         We are launching “ArtiStake”, a platform for supporting artists, with the aim of creating a new community of
         artists and a sustainable ecosystem as a DAO(Decentralized Autonomous Organization). By staking an artist you
         want to support, the platform will reward both the artist and the stakers with staking rewards. By learning
@@ -18,7 +17,7 @@ const About = () => {
         participate in community events, and so on. It is our sincere hope that this platform will help shape the
         relationship between artists, fans, and collectors, support crypto artists, and contribute to the ecosystem.
       </p>
-      <p className="text-white text-sm mb-4">
+      <p className="text-white mb-8">
         We are creating a sustainable ecosystem for artists, a new way of community, and a platform to support artists.
         When artists and developers belong to this community, they receive a community token. By holding this community
         token, they are granted various privileges and rights in the community. For example, they have priority to hold
@@ -29,47 +28,47 @@ const About = () => {
         rewards, depending on the amount and duration of their staking.
       </p>
 
-      <h3 className="text-white mb-4">---About community---</h3>
-      <p className="text-white text-sm mb-4">
+      <h3 className="text-white text-xl mb-8">---About community---</h3>
+      <p className="text-white mb-4">
         NFT & CRYPTO ART JAPAN is an open community of crypto artists based in Japan. We started our activities in
         February of 2021, and we are forming the community on Discord. We also hold study sessions on NFT at Clubhouse
         and NFT workshops for beginners on Zoom. We also make land (parcels) owned by us in the metaverse (mainly
         CryptoVoxels) available for artists, and help them open galleries and hold exhibitions. We are also running
         CryptoArtWeekAsia in Tokyo on a volunteer basis. In the future, we plan to make the entire community into a DAO.
       </p>
-      <p className="text-white text-sm mb-4">
+      <p className="text-white mb-4">
         The following is the press release and interview of the first physical NFT exhibition in Japan held on June 21,
         2021!
       </p>
-      <p className="text-white text-sm mb-4 underline">
+      <p className="text-white mb-4 underline">
         <a href={"https://prtimes.jp/main/html/rd/p/000000002.000081751.html"}>
           https://prtimes.jp/main/html/rd/p/000000002.000081751.html
         </a>
       </p>
-      <p className="text-white text-sm mb-4 underline">
+      <p className="text-white mb-4 underline">
         <a href={"https://finders.me/articles.php?id=2870"}>https://finders.me/articles.php?id=2870</a>
       </p>
-      <p className="text-white text-sm mb-4">We held physical NFT exhibition for CryptoArtWeekAsia in Tokyo!</p>
-      <p className="text-white text-sm mb-4 underline">
+      <p className="text-white mb-4">We held physical NFT exhibition for CryptoArtWeekAsia in Tokyo!</p>
+      <p className="text-white mb-4 underline">
         CryptoArtWeekAsia: <a href={"https://www.cryptoartweekasia.com/"}>https://www.cryptoartweekasia.com/</a>
       </p>
-      <p className="text-white text-sm mb-4 underline">
+      <p className="text-white mb-4 underline">
         Press Release:
         <a href={"https://prtimes.jp/main/html/rd/p/000000004.000081751.html"}>
           https://prtimes.jp/main/html/rd/p/000000004.000081751.html
         </a>
       </p>
-      <p className="text-white text-sm mb-4">
+      <p className="text-white mb-8">
         We hope to creatively shape the world with NFT, from Japan and with our friends around the world! Your support
         for the artists would be greatly appreciated!
       </p>
 
-      <h2 className="text-center text-white mb-4">-JP-</h2>
+      <h3 className="text-white text-xl mb-8">-JP-</h3>
 
-      <p className="text-white text-sm mb-4">
+      <p className="text-white mb-4">
         "ArtiStake"は、アーティストの支援のためのプラットフォームであり、アーティストベースのコミュニティです。
       </p>
-      <p className="text-white text-sm mb-4">
+      <p className="text-white mb-8">
         私たちはアーティストの新しいコミュニティのあり方、サステイナブルなエコシステムをDAOとして作ることを目的に、アーティストの支援のためのプラットフォーム"ArtiStake"を立ち上げます。
         このプラットフォームでは、自分の応援したいアーティストにステーキングをすることで、アーティストとステーカーそれぞれにリワードとしてステーキング報酬が発生します。アーティストの作品や、作品制作の様子、作品の背後にあるバックグラウンドストーリーを知ることで、アーティストの魅力をさらに知ることができ、応援したくなるような仕組みを提供します。
         アーティストをステーキングで支援してくださった方には、ステーキングの額と期間に応じて、アーティストのNFTをリワードとして受け取ることができるようになります。
@@ -78,8 +77,8 @@ const About = () => {
         例えば、私たちの所有するCryptovoxelsのparcelで展覧会を優先的に開催することができたり、コミュニティメンバー用の情報にアクセスできたり、コミュニティのイベントに参加できたり、などといった権利です。
         このプラットフォームにより、アーティストとファン、そしてコレクターの間の関係を形作り、クリプトアーティストの支援と、エコシステムに貢献することができればと切に願います。
       </p>
-      <h3 className="text-white mb-4">---コミュニティについて---</h3>
-      <p className="text-white text-sm mb-4">
+      <h3 className="text-white text-xl mb-8">---コミュニティについて---</h3>
+      <p className="text-white mb-4">
         NFT & CRYPTO ART JAPAN は日本をベースとしたクリプトアーティストのオープンなコミュニティです。
         2021年の2月に活動を開始し、Discordでコミュニティを形成しています。
         ClubhouseでNFTに関する勉強会をしたり、Zoomで初心者向けのNFTワークショップなども行っています。
@@ -87,30 +86,30 @@ const About = () => {
         また、CryptoArtWeekAsia in
         Tokyoの運営も、ボランティアで行っております。今後はコミュニティ全体をDAO化していく予定です。
       </p>
-      <p className="text-white text-sm mb-4">
+      <p className="text-white mb-4">
         以下は、2021/6/21 に行った、日本初のフィジカルNFT展覧会のプレスリリースと、インタビュー記事です！
       </p>
-      <p className="text-white text-sm mb-4 underline">
+      <p className="text-white mb-4 underline">
         <a href={"https://prtimes.jp/main/html/rd/p/000000002.000081751.html"}>
           https://prtimes.jp/main/html/rd/p/000000002.000081751.html
         </a>
       </p>
-      <p className="text-white text-sm mb-4 underline">
+      <p className="text-white mb-4 underline">
         <a href={"https://finders.me/articles.php?id=2870"}>https://finders.me/articles.php?id=2870</a>
       </p>
-      <p className="text-white text-sm mb-4">
+      <p className="text-white mb-4">
         そして2021/7/9 - 7/17の会期で CryptoArtWeekAsia in Tokyo　として、フィジカルNFT展覧会を企画制作しました！
       </p>
-      <p className="text-white text-sm mb-4 underline">
+      <p className="text-white mb-4 underline">
         <a href={"https://www.cryptoartweekasia.com/"}>https://www.cryptoartweekasia.com/</a>
       </p>
-      <p className="text-white text-sm mb-4 underline">
+      <p className="text-white mb-4 underline">
         <a href={"https://prtimes.jp/main/html/rd/p/000000004.000081751.html"}>
           https://prtimes.jp/main/html/rd/p/000000004.000081751.html
         </a>
       </p>
 
-      <p className="text-white text-sm mb-4">
+      <p className="text-white mb-4">
         私たちは、日本から、そしてワールドワイドに広がる仲間たちと
         NFTで世界をクリエイティブに形作っていきたいと願っています！
         アーティストのご支援、どうぞよろしくお願いいたします！
