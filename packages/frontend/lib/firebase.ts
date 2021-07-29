06f9d288f47f7bcc047f333bf98e070cb93194aa
kenta suhara <ponta.blockchain@gmail.com>
2021-07-29 15:09:33 +0900

    fix: firebase config

--- packages/frontend/lib/firebase.ts
+++ packages/frontend/lib/firebase.ts
@@ -1,14 +1,17 @@
 import firebase from "firebase/app";
 import "firebase/analytics";
 
-// const config = {
-//   authDomain: "artistake-prod.firebaseapp.com",
-//   projectId: "artistake-prod",
-//   storageBucket: "artistake-prod.appspot.com",
-//   messagingSenderId: "462334348518",
-//   appId: "1:462334348518:web:19bb698c6b13964ddf7f27",
-// };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5KGLwnSSHQ6AjO4LRIA6xXac0MdulDMc",
+  authDomain: "artistake-prod.firebaseapp.com",
+  projectId: "artistake-prod",
+  storageBucket: "artistake-prod.appspot.com",
+  messagingSenderId: "462334348518",
+  appId: "1:462334348518:web:19bb698c6b13964ddf7f27",
+  measurementId: "G-NSNQHLGFG5",
+};
 
-// firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 export default firebase;
